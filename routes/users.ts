79693f56def7f70cb5226451d43b1e52162e7a1c
Motti Bechhofer <MBechhofer@encoresupport.org>
2019-02-24 20:29:36 -0500

    added more routes to user api

--- routes/users.ts
+++ routes/users.ts
@@ -3,7 +3,25 @@
 const router: Router = Router();
 
 router.get("/user/:id", (request: Request, response: Response) => {
-    response.send('Hello world!');
+    let id = request.body.id;
+    let user = await getUserById(id);
+    response.send(user)
 });
+
+router.get("/user/:location", (request: Request, response: Response) => {
+    var location = request.body.location;
+    
+    //find users in 2km radius to current user location
+    var closeByUsers = await findUsersByLocation(location, 2);
+    response.send(closeByUsers)
+});
+
+
+router.post("/user/:id/rate", (request: Request, response: Response) => {
+    let rating = request.body.rating;
+    let userId = request.body.user.id;
+    await rateUser(userId, rating);
+})
+
 
 export { router };
