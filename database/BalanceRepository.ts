ebc995a2ef637fdb0e44206c0ca82f43e09b32eb
Motti Bechhofer <MBechhofer@encoresupport.org>
2019-04-08 23:29:02 -0400

    missing new line

--- database/BalanceRepository.ts
+++ database/BalanceRepository.ts
@@ -4,7 +4,8 @@ import { balance } from "../models/balance";
 export class BalanceRepository extends BaseRepository<balance> {
     find(item: balance): Promise<balance[]> {
         throw new Error("Method not implemented.");
-    } findOne(id: string): Promise<balance> {
+    }
+    findOne(id: string): Promise<balance> {
         throw new Error("Method not implemented.");
     }
 
