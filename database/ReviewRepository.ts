8322cbe32c7fc652c08aab213909507bdd7b142a
Motti Bechhofer <MBechhofer@encoresupport.org>
2019-04-17 11:56:29 -0400

    init ReviewService

---
+++ database/ReviewRepository.ts
@@ -0,0 +1,19 @@
+import { BaseRepository } from "./BaseRepository ";
+import { Review } from "../models/review";
+
+export class ReviewRepository extends BaseRepository<Review>
+{
+    update(item: Review): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    find(item: Review): Promise<Review[]> {
+        throw new Error("Method not implemented.");
+    }
+    findOne(id: string): Promise<Review> {
+        const review = this._items.find(r => r.createdById == id);
+        if (!review) {
+            return Promise.reject();
+        }
+        return Promise.resolve(review);
+    }
+}
---
+++ services/reviewService.ts
@@ -0,0 +1,20 @@
+import { ReviewRepository } from "../database/ReviewRepository";
+import { Review } from "../models/review";
+import { UserService } from "./userService";
+
+export class ReviewService {
+    private _reviewRepository!: ReviewRepository;
+    private _userService!: UserService;
+
+    getUserReviews(userId: string): Promise<Review> {
+        return this._reviewRepository.findOne(userId);
+    }
+    saveReview(review: Review): void {
+        this._reviewRepository.create(review);
+    }
+    async calculateUserRating(review: Review): Promise<void> {
+        const user = await this._userService.getUser(review.reviewdUserId);
+        var rating = +review.rating * user.rating;
+    }
+
+}
