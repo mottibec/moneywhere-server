8cb08a4643d97c02596d7a20b0176d39ecc69a83
Motti Bechhofer <MBechhofer@encoresupport.org>
2019-05-01 00:08:13 -0400

    init auth service

---
+++ services/authProvider.ts
@@ -0,0 +1,15 @@
+interface IAuthProvider {
+
+}
+
+export class LocalAuthProvider implements IAuthProvider {
+
+}
+
+export class GoogleAuthProvider implements IAuthProvider {
+
+}
+
+export class FacebookAuthProvider implements IAuthProvider {
+
+}
