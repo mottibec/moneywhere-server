4601343e68480f0ec6733f89f9b9d69ccd8a5ef3
Motti Bechhofer <MBechhofer@encoresupport.org>
2019-05-23 08:19:30 -0400

    add err handler

--- webserver/ExpressWebServer.ts
+++ webserver/ExpressWebServer.ts
@@ -1,5 +1,5 @@
 import { IWebServer } from "./IWebServer";
-import { Request, Response, Router, Application } from "express";
+import { Request, Response, Router, Application, NextFunction } from "express";
 import express from "express";
 import { injectable } from "inversify";
 
@@ -14,6 +14,7 @@ export default class ExpressWebServer implements IWebServer {
     }
     start(port: number, callback: Function) {
         this._app.use('/', this._router);
+        this._app.use(this.handleError);
         this._app.listen(port, callback);
     }
 
@@ -23,5 +24,10 @@ export default class ExpressWebServer implements IWebServer {
 
     registerPost(routeTemplate: string, callback: Function): void {
         this._router.post(routeTemplate, (request: Request, response: Response) => callback(request, response));
+    }
+    handleError(err: Error, req: Request, res: Response, next: NextFunction) {
+        console.log("err", err);
+        res.status(500);
+        res.render('error', { error: err });
     }
 }
